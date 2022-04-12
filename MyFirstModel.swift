d19e92c6566b53e618713f7287b2174553e2ee4e
ArtyomPotapov <artyom.potapov@icloud.com>
2022-04-12 18:46:06 +0300

    initial

---
+++ MyFirstModel.swift
@@ -0,0 +1,16 @@
+//
+//  MyFirstModel.swift
+//  MyFramework
+//
+//  Created by Artyom Potapov on 12.04.2022.
+//
+
+import Foundation
+
+public class MyFirstModel{
+    let title: String
+    
+    public init(title: String){
+        self.title = title
+    }
+}
